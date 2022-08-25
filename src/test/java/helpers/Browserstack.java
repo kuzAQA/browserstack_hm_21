5e3b21de7ca2d130e4755b9437da18e5aec8dfe7
kuzAQA <kuznetsov.public@yandex.ru>
2022-08-25 10:56:26 +0300

    добавлен owner в Browserstack.java

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,15 +1,18 @@
 package helpers;
 
+import config.ConfigAndroid;
+import org.aeonbits.owner.ConfigFactory;
+
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class Browserstack {
-
+    static ConfigAndroid config = ConfigFactory.create(ConfigAndroid.class);
     public static String videoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("bsuser_1cX9zb", "xgv7gi6aHJjVzzYDZg4X")
+                .auth().basic(config.userLogin(), config.userPassword())
                 .log().all()
                 .when()
                 .get(url)
